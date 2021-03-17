24672d6db227d8d2eb17e43b3627c2113226b7d4
Roushan Kumar <helloroushankumar@gmail.com>
2021-03-17 12:38:32 +0530

    Position of Recyclerview added

--- recycler/src/main/java/com/roushan/recycler/RecyclerAdapter.kt
+++ recycler/src/main/java/com/roushan/recycler/RecyclerAdapter.kt
@@ -5,13 +5,27 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
+import kotlin.collections.ArrayList
 
-class RecyclerAdapter(val list: MutableList<Any>, val resid: Int, val callback: RecyclerAction) :
+class RecyclerAdapter(val list: List<Any>, val resid: Int, val callback: RecyclerAction) :
     RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder>() {
 
+    var dlist: MutableList<Any> = mutableListOf()
+
+    init {
+        dlist = list as MutableList<Any>;
+    }
+
+
+    public fun update(ulist: List<Any>) {
+        dlist.clear()
+        dlist.addAll(ulist)
+        notifyDataSetChanged()
+    }
+
     inner class RecyclerHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        fun onBind(any: Any) {
-            callback.onAction(view, any)
+        fun onBind(position: Int) {
+            callback.onAction(view, dlist.get(position),position)
         }
     }
 
@@ -21,17 +35,17 @@ class RecyclerAdapter(val list: MutableList<Any>, val resid: Int, val callback: 
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return dlist.size
     }
 
     override fun onBindViewHolder(holder: RecyclerHolder, position: Int) {
-        holder.onBind(list.get(position))
+        holder.onBind(position)
     }
 
 }
 
 public interface RecyclerAction {
     fun onAction(
-        view: View, any: Any
+        view: View, any: Any,position:Int
     )
 }
